7b1efbf57faa9f6bcf4915dfd0a91b1550047440
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2018-06-17 19:58:08 -0400

    Restructure files
    - Created package wiki
    - Kept everything under the same package but files are kept under the
    same packages.

---
+++ wiki/handlers.go
@@ -0,0 +1,50 @@
+package wiki
+
+import (
+	"net/http"
+	"regexp"
+)
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+// SaveHandler is responsible of coordinating the saving of a Page.
+var SaveHandler = makeHandler(func(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+})
+
+// ViewHandler is responsible of coordinating the viewing of a Page.
+var ViewHandler = makeHandler(func(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+})
+
+// EditHandler is responsible of coordinating the display of the edit view of a page.
+var EditHandler = makeHandler(func(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+})
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
---
+++ wiki/views.go
@@ -0,0 +1,15 @@
+package wiki
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var templates = template.Must(template.ParseFiles("templates/edit.html", "templates/view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
